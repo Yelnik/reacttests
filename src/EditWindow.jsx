da3408bbd6b74589fe3383c5d0e8f60cd1ac0a5d
Justin K <yelnik9001@hotmail.com>
2019-03-12 20:41:19 -0400

    a

--- src/EditWindow.jsx
+++ src/EditWindow.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import ModalBody from "react-bootstrap/ModalBody";
 
-function EditWindow()  {
-  const[show, changeShow] = useState(false);
+function EditWindow(props) {
+  const [employee, updateEmployee] = useState(props.emp);
+  const [show, changeShow] = useState(props.show);
 
   function handleClose() {
+    props.update(employee);
     changeShow(false);
   }
 
@@ -13,41 +19,92 @@ function EditWindow()  {
     changeShow(true);
   }
 
-    return (
-      <div>
-        <Button
-          variant="success"
-          data-toggle="modal"
-          data-target="#exampleModal"
-          onClick={handleShow}
-        >
-          Launch demo modal
+  return (
+    <div>
+      <Button
+        variant="success"
+        data-toggle="modal"
+        data-target="#exampleModal"
+        onClick={handleShow}
+      >
+        Launch demo modal
         </Button>
-        <Modal
-          size="lg"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered
-          show={show} onHide={handleClose}
-        >
-          <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-vcenter">
-              Modal heading
+      <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={show} onHide={handleClose}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Edit Employee
             </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <h4>Centered Modal</h4>
-            <p>
-              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-              ac consectetur ac, vestibulum at eros.
-            </p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={handleClose}>Close</Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    );
+        </Modal.Header>
+        <Modal.Body>
+          <ModalFormBody />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
+
+function ModalFormBody() {
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+  </Button>
+    </Form>
+  );
 }
 
 export default EditWindow;
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from "react";
 import EditField from "./EditField";
 import BootstrapTable from "react-bootstrap/Table";
-import EditWindow from "./EditWindow"
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form"
+import Col from "react-bootstrap/Col"
 
 class Table extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tableData: [], datAmt: props.dataNum || "10" };
+    this.state = { tableData: [], datAmt: props.dataNum || "10", show: "false" };
 
     this.refreshResults = this.refreshResults.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+
+    this.editWin = this.editWin.bind(this);
+  }
+
+  handleClose() {
+    //props.update(employee);
+    //changeShow(false);
+  }
+
+  handleShow() {
+    this.setState( { show: "true" });
+  }
+
+  editWin(e) {
+    
+    //EditWindow({ emp: e, update: this.updateEmployee });
   }
 
   async componentDidMount() {
@@ -25,9 +45,15 @@ class Table extends React.Component {
   rowClick(id) {
     let list = this.state.tableData.slice();
     let personObj = list.find(person => person.cell == id);
-    personObj.name.first = "Rick";
-    this.setState({ tableData: list });
-    console.log(personObj);
+    this.setState({ show: "true" });
+
+    
+    // <div>
+    // <EditWindow emp={personObj} update={this.updateEmployee}/>
+    // </div>
+    // personObj.name.first = "Rick";
+    //this.setState({ tableData: list });
+    //console.log(personObj);
   }
 
   async refreshResults(amount) {
@@ -41,7 +67,34 @@ class Table extends React.Component {
 
     return (
       <div id="tableDiv">
-        <EditWindow />
+        <div>
+          <Button
+            variant="success"
+            data-toggle="modal"
+            data-target="#exampleModal"
+            onClick={this.handleShow}
+          >
+            Launch demo modal
+        </Button>
+          <Modal
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+            show={this.show} onHide={this.handleClose}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Edit Employee
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {/* <ModalFormBody /> */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleClose}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
         <EditField onclick={this.refreshResults} dataNum={this.state.datAmt} />
         <BootstrapTable striped bordered hover variant="dark" id="empTbl">
           <thead>
@@ -70,6 +123,62 @@ class Table extends React.Component {
       </div>
     );
   }
+}
+
+function ModalFormBody() {
+  return (
+    <Form>
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+
+      <Form.Group controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+
+      <Form.Row>
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Control as="select">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Form.Row>
+
+      <Form.Group id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+  </Button>
+    </Form>
+  );
 }
 
 export default Table;
